2be20f5286d31ccb1f45fbc09b15e2d6e5606061
Jaquavia <jaquaviawalker@gmail.com>
2023-08-29 21:27:00 -0400

    start of js code

---
+++ script.js
@@ -0,0 +1,9 @@
+const start_button = document.querySelector('.start')
+const stop_button = document.querySelector('.stop')
+const reset_button = document.querySelector('.reset')
+let timer = document.getElementById('time')
+
+
+start_button.addEventListener('click', function () {
+    alert('hello')
+})
